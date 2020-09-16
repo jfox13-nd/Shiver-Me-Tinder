29811effc1b5466f122c927f177b4ff0e4d2db50
Jack Fox <jfox13@nd.edu>
2020-09-16 15:16:31 -0400

    Removed dead code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
@@ -16,8 +14,6 @@ import { NewProfileComponent } from './new-profile/new-profile.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CrisisListComponent,
-    HeroesListComponent,
     PageNotFoundComponent,
     HomeComponent,
     NavComponent,
